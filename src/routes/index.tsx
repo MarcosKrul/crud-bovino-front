15bb076992ba126225cb21c30c5e19d3830be73d
Marcos Renan Krul <marcoskrul2k@gmail.com>
2021-04-22 09:06:58 -0300

    utilizacao dos componentes para roteamento

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import MainPage from "../pages/main";
+import List from "../components/list";
+import Home from "../components/home";
+import Create from "../components/create";
+
 const Routes: React.FC = () => (
     <Switch>
-        <Route path="/" exact component={() => (<h1>pagina /</h1>)}/>
-        <Route path="/teste" component={() => (<h1>pagina teste</h1>)}/>
+        <MainPage>
+            <Route path="/list" component={List}/>
+            <Route exact path="/" component={Home}/>
+            <Route path="/create" component={Create}/>
+        </MainPage>
     </Switch>
-)
+);
 
 export default Routes;
