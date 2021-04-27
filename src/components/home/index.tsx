bad861dd74ad4acb8a3c7050759fc570ad837e97
Marcos Renan Krul <marcoskrul2k@gmail.com>
2021-04-27 15:51:44 -0300

    componente para pagina home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,9 +1,48 @@
 import React from "react";
 
-import {} from "./styles";
+import { Container } from "./styles";
 
 const Home: React.FC = () => (
-    <h1>Home</h1>
+    <Container>
+        <h1>Sistema para controle de bovinos</h1>
+        <h2>Resolução do teste teórico e prático para treinamento do estágio na empresa Rerum Engenharia de Sistemas</h2>
+        <h3>Desenvolvido em 04/2021 por Marcos Renan Krul {'<marcoskrul2k@gmail.com>'}</h3>
+
+        <p>Repositórios remotos contendo o código fonte</p> <br />
+        <ul>
+            <li>
+                <a href="https://github.com/MarcosKrul/crud-bovino-front" target="_blank">Front-end</a>
+            </li>
+            <li>
+                <a href="https://github.com/MarcosKrul/crud-bovino-back" target="_blank">Back-end</a>
+            </li>
+        </ul>
+        
+        <p>Funcionalidades do sistema</p> <br />
+        <ul>
+            <li>Cadastro de bovinos</li>
+            <li>Listagem de bovinos</li>
+            <li>Busca de bovinos ou por nome ou por brinco</li>
+        </ul>
+
+        <p>Características de um bovino</p> <br />
+        <ul>
+            <li>Raça</li>
+            <li>Situação atual</li>
+            <li>Data de nascimento</li>
+            <li>Brinco do bovino mãe</li>
+            <li>Brinco do bovino pai</li>
+            <li>Nome (máx. 20 caracteres)</li>
+            <li>Brinco (máx. 8 caracteres)</li>
+            <li>
+                Para fêmeas
+                <ul>
+                    <li>Data de prenhez</li>
+                    <li>Data do último parto</li>
+                </ul>
+            </li>
+        </ul>
+    </Container>
 )
 
 export default Home;
--- src/components/home/styles.ts
+++ src/components/home/styles.ts
@@ -1,1 +1,26 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    margin: 40px 0px;
+    margin-left: 40px;
+    align-items: start;
+    flex-direction: column;
+    justify-content: center;
+
+    p { margin-top: 50px; }
+
+    ul { list-style-type: disc; }
+
+    li { margin-left: 50px; }
+
+    li a { 
+        color: black;
+        text-decoration: none; 
+    }
+
+    li a:hover {
+        color: blue;
+        font-size: 1.1rem;
+    }
+`
